475c0632a964f762c443ca321691b8233622d4d5
Alex Fernandez <51440246+aleexfr1@users.noreply.github.com>
2020-10-29 17:15:15 +0100

    add unit 4 exercises

--- UNIT 4/events.js
+++ UNIT 4/events.js
@@ -17,8 +17,11 @@ btn.onclick = function() {  // Le ponemos una funcion al hacer click en el boton
 // Creamos la funcion y la asociamos junto al tipo de evento en el boton desde el HTML
 function random(number) {
     return Math.floor(Math.random() * (number+1)); // Crea numeros aleatorios
-    
+
 function bgChange() {
     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
     document.body.style.backgroundColor = rndCol;
-  }
+
+}
+
+ }
