db56e9538d0525dfe0f98e17943efefe970ab239
aleexfr1 <a.fdez@outlook.com>
2020-12-14 10:11:19 +0100

    ajax example

---
+++ AJAX/index.js
@@ -0,0 +1,15 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //document.getElementById("demo").innerHTML += this.responseText;
+            datos = JSON.parse(this.responseText);
+
+            for(i=0; i<datos.listaNotas.length; i++)
+            
+            alert(notas.listaNotas[i].titulo)
+        }
+    };
+    xhttp.open("GET", "texto.txt", true);
+    xhttp.send();
+}
